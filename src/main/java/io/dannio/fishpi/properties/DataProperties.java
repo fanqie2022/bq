da685ffe5ce341889a038cf45c2bbb24e1648991
dannio <lendoon@gmail.com>
2022-02-13 23:18:29 +0800

    :art: send fishpi gif file using by ffmpeg convert telegram animation video

--- src/main/java/io/dannio/fishpi/properties/DataProperties.java
+++ src/main/java/io/dannio/fishpi/properties/DataProperties.java
@@ -4,6 +4,9 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import java.io.File;
+
 @Component
 @ConfigurationProperties("app.data")
 public @Data
@@ -13,4 +16,17 @@ class DataProperties {
 
     private String fishpi;
 
+
+    @PostConstruct
+    public void initialize() {
+        if (mkdirIfNotExists(new File(this.telegram)) && mkdirIfNotExists(new File(this.fishpi))) {
+            throw new RuntimeException("app data store can not initialize");
+        }
+    }
+
+
+    private boolean mkdirIfNotExists(File file) {
+        return file.exists() || file.mkdirs();
+
+    }
 }
