519ef29538057fecd3bc5f7d153a98dd93f51100
dannio <lendoon@gmail.com>
2022-05-20 14:56:23 +0800

    🎨 update generate204 to get

--- src/main/java/io/dannio/fishpi/api/Api.java
+++ src/main/java/io/dannio/fishpi/api/Api.java
@@ -2,7 +2,7 @@ package io.dannio.fishpi.api;
 
 import org.springframework.stereotype.Controller;
 
-import javax.ws.rs.POST;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
@@ -12,7 +12,7 @@ import static javax.ws.rs.core.Response.Status.NO_CONTENT;
 @Controller
 public class Api {
 
-    @POST
+    @GET
     @Path("/generate_204")
     public Response foo() {
 
